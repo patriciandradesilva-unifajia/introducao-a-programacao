c039bc7ddd92eeb11bddaa608ecd38b960a183ed
Patrícia Andrade da Silva <financeiro.patriciasilva@unieduk.com.br>
2026-02-26 21:25:49 -0300

    Add files via upload

---
+++ potencializacao.py
@@ -0,0 +1,15 @@
+#potencialização
+
+print ("##############")
+print ("   potenciação    ")
+print ("##############")
+
+a = input ("entre com o primeiro numero e tecle enter")
+b = input ("entre com o segundo numero e tecle enter")
+
+a = int(a)
+b = int(b)
+
+print (a**b)
+
+input()
